850a5fb2802c6c2cf9688d86ab91648539ea847e
levi97 <30956324+levi97@users.noreply.github.com>
2019-07-17 15:45:52 -0700

    Update navigate2D_env.py

--- gym_navigate2D/envs/navigate2D_env.py
+++ gym_navigate2D/envs/navigate2D_env.py
@@ -94,11 +94,12 @@ class navigate2DEnv(gym.Env):
             obs = self.state
             reward = -0.1
         else:
+            right_choice = np.square(self.x_index - 8) + np.square(self.y_index - 58) > np.square(tmp_x_index - 8) + np.square(tmp_y_index - 58)
             self.x_index = tmp_x_index
             self.y_index = tmp_y_index
             self.done = 7 < self.x_index < 9 and 55 < self.y_index < 61
             obs = STATE_ARRAY[:, :, self.x_index, self.y_index, np.newaxis]
-            reward = -0.1*(1 - self.done) + self.done
+            reward = 0.1*(1 - self.done)*(-1 + 2*right_choice) + 10*self.done
 
         self.nbEpisode = self.nbEpisode + 1*self.done
 
