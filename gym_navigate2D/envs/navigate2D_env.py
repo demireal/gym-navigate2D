2b61a7bce172f1c3d99e264518ac014a0daf3374
levi97 <30956324+levi97@users.noreply.github.com>
2019-07-17 19:57:05 -0700

    Update navigate2D_env.py

--- gym_navigate2D/envs/navigate2D_env.py
+++ gym_navigate2D/envs/navigate2D_env.py
@@ -87,9 +87,14 @@ class navigate2DEnv(gym.Env):
         elif action == 3:  # Up
             tmp_x_index = self.x_index
             tmp_y_index = self.y_index + 1
+            while(np.abs(DISTANCES[self.x_index, tmp_y_index] - DISTANCES[self.x_index, self.y_index]) < 0.38):
+              tmp_y_index += 1
         else:  # Down
             tmp_x_index = self.x_index
             tmp_y_index = self.y_index - 1
+            while(np.abs(DISTANCES[self.x_index, tmp_y_index] - DISTANCES[self.x_index, self.y_index]) < 0.38):
+              tmp_y_index -= 1
+              
         if tmp_x_index < 0 or tmp_x_index > X_STATES - 1 or tmp_y_index < 0 or tmp_y_index > Y_STATES - 1:
             obs = self.state
             reward = -0.1
