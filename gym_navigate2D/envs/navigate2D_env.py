4905865b29e83ac57d13729494faf5b4eaafd677
Ilker <ilkerdemirel97@gmail.com>
2019-07-27 23:26:05 -0700

    Initial commit

--- gym_navigate2D/envs/navigate2D_env.py
+++ gym_navigate2D/envs/navigate2D_env.py
@@ -1,16 +1,10 @@
 import gym
-import imageio
-import glob
 import numpy as np
 import random
 import pandas as pd
-
-
 from time import sleep
 from IPython.display import clear_output
-from gym import error, spaces, utils
-from gym.utils import seeding
-from PIL import Image
+from gym import spaces
 from matplotlib import pyplot as plt
 
 # Each state is an image. State space is 2D.
@@ -19,9 +13,9 @@ NUM_OF_ACTIONS = 5
 
 DF = pd.read_csv('/content/drive/My Drive/UBC Research/xy.csv')
 DISTANCES = DF.values
-for index_1, str_arr in enumerate(DISTANCES):
-  for index_2, strr in enumerate(str_arr):
-     DISTANCES [index_1, index_2] = float(strr.replace(',','.'))
+for index_1, entry_array in enumerate(DISTANCES):
+    for index_2, entry in enumerate(entry_array):
+        DISTANCES [index_1, index_2] = float(entry.replace(',', '.'))
 
       
 class navigate2DEnv(gym.Env):
@@ -29,18 +23,18 @@ class navigate2DEnv(gym.Env):
 
     def __init__(self, path, is_test=0):
         self.state_array = np.load(path)
-        self.x_states = self.state_array.shape[2]
-        self.y_states = self.state_array.shape[3]
-        self.input_shape = (self.state_array.shape[0], self.state_array.shape[1], 1)
+        self.x_states = self.state_array.shape[0]
+        self.y_states = self.state_array.shape[1]
+        self.input_shape = (self.state_array.shape[2], self.state_array.shape[3], 1)
         self.is_test = is_test
         self.x_index = random.randint(0, self.x_states - 1)
         self.y_index = random.randint(0, self.y_states - 1)
-        self.state = self.state_array[:, :, self.x_index, self.y_index, np.newaxis]
+        self.state = self.state_array[self.x_index, self.y_index, np.newaxis, :, :]
         self.flag = False
         self.done = False
         self.nbEpisode = 1
         self.action_space = spaces.Discrete(NUM_OF_ACTIONS)
-        self.observation_space = spaces.Box(low=0, high=1, shape=self.input_shape, dtype='float32')
+        self.observation_space = spaces.Box(low=0, high=255, shape=self.input_shape, dtype='numpy.uint8')
 
     def step(self, action):
         self.state, reward = self.take_action(action)
@@ -49,30 +43,30 @@ class navigate2DEnv(gym.Env):
     def reset(self):
         print('Episode: ' + str(self.nbEpisode))
         if self.nbEpisode < 10 and self.is_test == 0:
-          self.x_index = random.randint(5, 9)
-          self.y_index = random.randint(50, 66)
+            self.x_index = random.randint(5, 9)
+            self.y_index = random.randint(50, 66)
         elif self.nbEpisode < 25 and self.is_test == 0:
-          self.x_index = random.randint(4, 11)
-          self.y_index = random.randint(40, 76)
+            self.x_index = random.randint(4, 11)
+            self.y_index = random.randint(40, 76)
         elif self.nbEpisode < 45 and self.is_test == 0:
-          self.x_index = random.randint(3, 13)
-          self.y_index = random.randint(30, 86)
+            self.x_index = random.randint(3, 13)
+            self.y_index = random.randint(30, 86)
         elif self.nbEpisode < 70 and self.is_test == 0:
-          self.x_index = random.randint(2, 15)
-          self.y_index = random.randint(20, 96)
+            self.x_index = random.randint(2, 15)
+            self.y_index = random.randint(20, 96)
         elif self.nbEpisode < 100 and self.is_test == 0:
-          self.x_index = random.randint(1, 17)
-          self.y_index = random.randint(10, 106)
+            self.x_index = random.randint(1, 17)
+            self.y_index = random.randint(10, 106)
         else:
-          self.x_index = random.randint(0, self.x_states - 1)
-          self.y_index = random.randint(0, self.y_states - 1)
-        self.state = self.state_array[:, :, self.x_index, self.y_index, np.newaxis]
+            self.x_index = random.randint(0, self.x_states - 1)
+            self.y_index = random.randint(0, self.y_states - 1)
+        self.state = self.state_array[self.x_index, self.y_index, np.newaxis, :, :]
         self.flag = False
         self.done = False
         return self.state
 
     def render(self, mode='human'):
-        plt.imshow(self.state[:, :, 0], cmap='gray', vmin=0, vmax=255)
+        plt.imshow(self.state[0, :, :], cmap='gray', vmin=0, vmax=255)
         plt.show()
         print('Position: ( ' + str(self.x_index) + ', ' + str(self.y_index) + ')')
         sleep(1)
@@ -104,7 +98,7 @@ class navigate2DEnv(gym.Env):
             self.y_index = tmp_y_index
             self.flag = 5 < self.x_index < 9 and 54 < self.y_index < 62
             self.done = self.flag and action == 0
-            obs = self.state_array[:, :, self.x_index, self.y_index, np.newaxis]
+            obs = self.state_array[self.x_index, self.y_index, np.newaxis, :, :]
             reward = 0.1*(1 - self.done)*(-1 + 2*right_choice) + self.done
         self.nbEpisode = self.nbEpisode + 1*self.done
 
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='gym_navigate2D',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'pillow', 'imageio', 'matplotlib', 'ipython', 'pandas']
+      install_requires=['gym', 'numpy', 'matplotlib', 'ipython', 'pandas']
 )
 
