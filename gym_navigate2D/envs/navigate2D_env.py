24d913a2dd2c92a90731db9eb6429fc6fa29387f
levi97 <30956324+levi97@users.noreply.github.com>
2019-07-30 16:45:15 -0700

    Update navigate2D_env.py

--- gym_navigate2D/envs/navigate2D_env.py
+++ gym_navigate2D/envs/navigate2D_env.py
@@ -11,10 +11,10 @@ from matplotlib import pyplot as plt
 
 PHI_MAX = 30
 
-X_STATES = 4001
+X_STATES = 400
 TILT_STATES = 101
 NUM_OF_ACTIONS = 5
-IN_DIM = (1, 50, 501)
+IN_DIM = (1, 50, 50)
 MASK_FILE = '/content/drive/My Drive/UBC Research/mask.png'
 INTERPOLATION = cv2.INTER_NEAREST
 
